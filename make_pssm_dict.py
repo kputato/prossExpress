864acc041f102b47952cbda82b215dd45b0170d4
kputato <karen.c.pu@vanderbilt.edu>
2023-07-21 17:57:24 +0200

    results for 6md5 and 7nei

--- make_pssm_dict.py
+++ make_pssm_dict.py
@@ -11,11 +11,10 @@ import json
 def softmax(x, T):
     return np.exp(x/T)/np.sum(np.exp(x/T), -1, keepdims=True)
 
-def parse_pssm(path):
+def parse_pssm(path, seq_len):
     data = pd.read_csv(path, skiprows=2)
     floats_list_list = []
-    #KP: change it manually --> 8 should be 4, 263 is length of sequence
-    for i in range(263):
+    for i in range(seq_len):
         str1 = data.values[i][0][8:]
         floats_list = []
         for item in str1.split():
@@ -24,43 +23,45 @@ def parse_pssm(path):
     np_lines = np.array(floats_list_list)
     return np_lines
 
-np_lines = parse_pssm('.temp/pssm.txt')
 
-mpnn_alphabet = 'ACDEFGHIKLMNPQRSTVWYX'
-input_alphabet = 'ARNDCQEGHILKMFPSTWYV'
+def make_dict(seq_len):
+    np_lines = parse_pssm('.temp/pssm.txt', seq_len)
 
-permutation_matrix = np.zeros([20,21])
-for i in range(20):
-    letter1 = input_alphabet[i]
-    for j in range(21):
-        letter2 = mpnn_alphabet[j]
-        if letter1 == letter2:
-            permutation_matrix[i,j]=1.
+    mpnn_alphabet = 'ACDEFGHIKLMNPQRSTVWYX'
+    input_alphabet = 'ARNDCQEGHILKMFPSTWYV'
 
-pssm_log_odds = np_lines[:,:20] @ permutation_matrix
-pssm_probs = np_lines[:,20:40] @ permutation_matrix
+    permutation_matrix = np.zeros([20,21])
+    for i in range(20):
+        letter1 = input_alphabet[i]
+        for j in range(21):
+            letter2 = mpnn_alphabet[j]
+            if letter1 == letter2:
+                permutation_matrix[i,j]=1.
 
-X_mask = np.concatenate([np.zeros([1,20]), np.ones([1,1])], -1)
+    pssm_log_odds = np_lines[:,:20] @ permutation_matrix
+    pssm_probs = np_lines[:,20:40] @ permutation_matrix
 
-def softmax(x, T):
-    return np.exp(x/T)/np.sum(np.exp(x/T), -1, keepdims=True)
+    X_mask = np.concatenate([np.zeros([1,20]), np.ones([1,1])], -1)
 
-#Load parsed PDBs:  
-with open('.temp/parsed_pdbs.jsonl', 'r') as json_file:
-    json_list = list(json_file)
+    def softmax(x, T):
+        return np.exp(x/T)/np.sum(np.exp(x/T), -1, keepdims=True)
 
-my_dict = {}
-for json_str in json_list:
-    result = json.loads(json_str)
-    all_chain_list = [item[-1:] for item in list(result) if item[:9]=='seq_chain']
-    pssm_dict = {}
-    for chain in all_chain_list:
-        pssm_dict[chain] = {}
-        pssm_dict[chain]['pssm_coef'] = (np.ones(len(result['seq_chain_A']))).tolist() #a number between 0.0 and 1.0 specifying how much attention put to PSSM, can be adjusted later as a flag
-        pssm_dict[chain]['pssm_bias'] = (softmax(pssm_log_odds-X_mask*1e8, 1.0)).tolist() #PSSM like, [length, 21] such that sum over the last dimension adds up to 1.0
-        pssm_dict[chain]['pssm_log_odds'] = (pssm_log_odds).tolist()
-    my_dict[result['name']] = pssm_dict
+    #Load parsed PDBs:  
+    with open('.temp/parsed_pdbs.jsonl', 'r') as json_file:
+        json_list = list(json_file)
 
-#Write output to:    
-with open('.temp/pssm_dict.jsonl', 'w') as f:
-    f.write(json.dumps(my_dict) + '\n')
+    my_dict = {}
+    for json_str in json_list:
+        result = json.loads(json_str)
+        all_chain_list = [item[-1:] for item in list(result) if item[:9]=='seq_chain']
+        pssm_dict = {}
+        for chain in all_chain_list:
+            pssm_dict[chain] = {}
+            pssm_dict[chain]['pssm_coef'] = (np.ones(len(result['seq_chain_A']))).tolist() #a number between 0.0 and 1.0 specifying how much attention put to PSSM, can be adjusted later as a flag
+            pssm_dict[chain]['pssm_bias'] = (softmax(pssm_log_odds-X_mask*1e8, 1.0)).tolist() #PSSM like, [length, 21] such that sum over the last dimension adds up to 1.0
+            pssm_dict[chain]['pssm_log_odds'] = (pssm_log_odds).tolist()
+        my_dict[result['name']] = pssm_dict
+
+    #Write output to:    
+    with open('.temp/pssm_dict.jsonl', 'w') as f:
+        f.write(json.dumps(my_dict) + '\n')
--- runMPNN.py
+++ runMPNN.py
@@ -1,4 +1,4 @@
-# Design a protein using MPNN and other intermediate steps:
+#   Design a protein using MPNN and other intermediate steps:
 #   Produce a PSSM matrix to ensure that conserved areas of the protein aren't altered
 #   Remove signal peptide (if present)
 #   Run MPNN to obtain individual mutations
@@ -8,48 +8,49 @@
 
 from colabfold.colabfold import run_mmseqs2
 from Bio import SeqIO, AlignIO
-import subprocess
+from colabdesign.mpnn import mk_mpnn_model
+from colabdesign.mpnn.model import aa_order
 from io import StringIO
 import os
+from os import listdir
 import shutil
-import numpy as np
 import json
 from Bio import SeqIO
 import subprocess
 from collections import defaultdict
 import pandas as pd
-import xml.etree.ElementTree as ET
-import Bio.PDB as bpdb
-import requests
-from langchain.tools import BaseTool
+import time
+from multiprocessing.pool import ThreadPool
+import make_pssm_dict as prep_mpnn
 
-class mpnnDesignTool():
-    name = "mpnn_design"
-    description = "This tool will be used after a PDB file is successfully extracted. We will first run MSA against the protein \
-sequence to find conserved areas, resulting in a PSSM matrix representation. If the protein contains a signal peptide, the signal peptide \
-will be removed. This tool will then iterate through the protein sequence and run MPNN, along with the PSSM matrix, on the protein, \
-where each iteration will only allow MPNN to design a single specific position. The individual mutations are scored with MPNN and ranked \
-and the top five mutations will be placed in the first designed sequence. The next five mutations will then be iteratively added, in addition \
-to the first five mutatios, creating five more designed sequences. This tool is expected to place the designed sequences in a json file."
 
-    def __str__(self):
-        return f"{self.name}: {self.description}"
+
+class runMPNN():
     
     def __init__(self, pdb_path: str):
-        self.target = pdb_path
+        self.target = ""
         self.name = pdb_path.split("/")[-1].replace(".pdb","")
+        self.native = ""
+        self.seq_len = 0
+        
         
     def run(self):
             
         # create a temp file for this tool that will be deleted later
         os.makedirs(".temp", exist_ok=True)
         
-        # get sequence from pdb file and make fasta
-        chain = {record.id: str(record.seq) for record in SeqIO.parse(self.target, 'pdb-seqres')}
-        query = "".join(list(chain.values()))
+        # clean pdb and get native sequence and length
+        subprocess.run(f"python rosetta_helper/clean_pdb.py example_run/{self.name}.pdb A", shell=True)
+        
+        # update native sequence and length
+        with open(f"{self.name}_A.fasta", "r") as fasta:
+            self.native = fasta.readlines()[-1].replace("\n","")
+            self.seq_len = len(self.native)
+            self.target = f"{self.name}_A.pdb"
+            self.name = self.name + "_A"
 
         # run sequence alignment using colabdesign api
-        run_mmseqs2(query, self.name)
+        run_mmseqs2(self.native, self.name)
 
         # find and parse a3m file to write to string
         seq_str = ''
@@ -73,10 +74,7 @@ to the first five mutatios, creating five more designed sequences. This tool is 
         for i in range(len(reference)):
             if reference[i] == "-":
                 to_del.append(i)
-
-        # find signal peptide as well
-        start, end = self._get_peptide(self.name)
-
+                
         for align in alignment:
             pos = {}
             for i in range(len(align.seq)):
@@ -84,99 +82,27 @@ to the first five mutatios, creating five more designed sequences. This tool is 
             for key in to_del:
                 pos.pop(key)
             align.seq = "".join(list(pos.values()))
-            align.seq = align.seq [:start-1] + align.seq[end:] # remove signal peptide
-
-        # remove signal peptide from pdb
-        self._process_pdb(pdb=self.target, start_resi=start, end_resi=end)
-
-        # create a fasta file for the pdb
-        f = open(f"./.temp/{self.name}.fasta", "w")
-        with open(self.target, 'r') as pdb_file:
-            for record in SeqIO.parse(pdb_file, 'pdb-atom'):
-                f.write('>wildtype\n')
-                f.write(str(record.seq))
-        f.close()
-
+        
         # run MPNN and update json database
-        self._getPSSM(alignment=alignment, uniprot=self.name)
-        top_ten = self._runMPNN(len(str(record.seq)))
+        self._getPSSM(alignment=alignment)
+        status = self._runMPNN()
+        print(status)
 
         # obtain mutation sequences for top 10 mutations
-        print(self._getSeqs(top_ten))
-
-        # delete temp folder and MSA folder
-        shutil.rmtree("./.temp")
+        data = self._getSeqs()
+        
+        os.makedirs("results", exist_ok=True)
+        data.to_csv(f"results/{self.name}.csv", index=False)
+        
+        # # delete temp folder and MSA folder
+        # shutil.rmtree("./.temp")
         shutil.rmtree(f"./{self.name}_env")
+        # TODO: clean up .fasta 'n .pdb
 
-        return "MPNN has successfully finished making designed sequences. The designed sequences are \
-available in the json file."
+        return "MPNN has successfully finished making designed sequences."
 
-    def _get_peptide(self, uniprot_id):
-    # Retrieve positions of the signal peptide inthe protein
-
-        url = f"https://www.uniprot.org/uniprot/{uniprot_id}.xml"
-
-        # Send a GET request to the UniProt API
-        response = requests.get(url)
-
-        # Check if the request was successful
-        if response.ok:
-            # Parse the XML response
-            xml_data = response.content.decode()
-
-            root = ET.fromstring(xml_data)
-
-            # Find the feature elements
-            feature_elements = root.findall(".//{http://uniprot.org/uniprot}feature")
-
-            # Find the signal peptide feature
-            for elements in feature_elements:
-                if elements.get('type') == "signal peptide":
-                    start = int(elements[0][0].attrib['position'])
-                    end = int(elements[0][1].attrib['position'])
-            return start, end  
-        
-    def _process_pdb(self, pdb, start_resi, end_resi):
-        # modify pdb file in place to remove signal peptide
-
-        s = bpdb.PDBParser().get_structure("tmp", pdb)
-
-        # subclass that will tell pdb output class which residues to save
-        class ResSelect(bpdb.Select):
-            def accept_residue(self, res):
-                if res.id[1] >= start_resi and res.id[1] <= end_resi:
-                    return False
-                else:
-                    return True
-                
-        io = bpdb.PDBIO()
-        io.set_structure(s)
-        io.save(pdb, ResSelect())
-        
-        # renumber the pdb file to account for signal peptide removal
-        io = bpdb.PDBIO()
-        parser = bpdb.PDBParser()
-
-        my_pdb_structure = parser.get_structure('test', pdb)
-
-        # renumber residue in my_pdb_structure
-        residue_N = 1
-        for model in my_pdb_structure:
-            for chain in model:
-                    for residue in chain:
-                        print(residue.id)
-                        if 'A' in residue.id[2]:
-                            residue.id = (residue.id[0], residue_N-1, residue.id[2])       
-                        else:
-                            residue.id = (residue.id[0], residue_N, residue.id[2])
-                            residue_N += 1
-                
-        io.set_structure(my_pdb_structure) 
-        io.save(pdb,  preserve_atom_numbering=True) 
-
-        return None
     
-    def _getPSSM(self, alignment, uniprot):
+    def _getPSSM(self, alignment):
         """Get a PSSM matrix by running psi blast against MSA object; process it for MPNN"""
         
         # Write MSA to FASTA file and turn it into a database
@@ -190,141 +116,149 @@ available in the json file."
         subprocess.run("makeblastdb -in .temp/database.fasta -dbtype prot -out .temp/db", shell=True)
 
         # prepare for psi blast pssm
-        query = f".temp/{uniprot}.fasta"
+        query = f"{self.name}.fasta"
 
         # obtain a PSSM matrix using psi blast
         subprocess.run(f'psiblast -query {query} -db .temp/db -num_iterations 3 -out_ascii_pssm \
                        .temp/pssm.txt -outfmt 0', shell=True)
+        
+        return "PSSM matrix has been successfully produced and saved in .temp/pssm.txt"
 
-    def _runMPNN(self, seq_len):
+    def _task(self, i):
+        # multithreaded process of runing MPNN
+        
+        pos_data = defaultdict(list)
+        
+        # get PSSM matrix
+        with open('.temp/pssm_dict.jsonl','r') as pssm:
+            temp = json.load(pssm)
+            bias = temp[self.name]["A"]["pssm_bias"]
+            
+        bias_matrix = bias[i]
+        pos = i + 1
+        
+        if i < 1:
+            fixed = f"{pos+1}-{len(bias)}"
+        elif pos >= len(bias):
+            fixed = f"1-{i}"
+        elif i == 1:
+            fixed = f"1, {pos+1}-{len(bias)}"
+        elif ((pos+1) == len(bias)):
+            fixed = f"1-{i},{len(bias)}"
+        else:
+            fixed = f"1-{i},{pos+1}-{len(bias)}"
+            
+        # create mpnn model
+        mpnn_model = mk_mpnn_model()
+        mpnn_model.prep_inputs(pdb_filename=self.target, 
+                            fix_pos=fixed)
+        
+        # adjust PSSM probabilities
+        alphabet = 'ACDEFGHIKLMNPQRSTVWY'
+        for aa in range(len(alphabet)):
+            mpnn_model._inputs["bias"][i,aa_order[alphabet[aa]]] = bias_matrix[aa]
+        
+        samples = mpnn_model.sample_parallel(batch=5)
+        
+        # record the data
+        ctr = 0
+        for sequence in samples["seq"]:
+            for j in range(self.seq_len):
+                if self.native[j] != sequence[j]:
+                    pos_data["sequence"].append(sequence)
+                    mut = f"{self.native[j]}    {sequence[j]}" # record what native position has been mutated to
+                    pos_data["mutation"].append(mut) 
+                    pos_data["score"].append(samples["score"][ctr])
+            ctr += 1   
+        
+        if pos_data:
+            df = pd.DataFrame(pos_data)
+            df['score'] = pd.to_numeric(df['score'])
+            df2 = df.sort_values(by='score', ascending=False).reset_index(drop=True)
+            df3 = df2.head(1)
+            df3.to_csv(f".temp/csv/{self.name}_{i}.csv", index=False, header=False)
+            
+        return "One position has been designed, and the amino acid mutation that led \
+            to the greatest improvement in thermostability has been recorded."
+
+    def _runMPNN(self):
         """
         Run helper scripts and protein MPNN
         """
         
-        pdb_path = "/".join((self.target).split('/')[:-1])
+        os.makedirs(".temp/csv", exist_ok=True)
 
-        #Run helper scripts to set up for MPNN
+        # Run helper scripts to set up for MPNN
         subprocess.run(f"python ProteinMPNN/helper_scripts/parse_multiple_chains.py \
-                       --input_path {pdb_path} \
+                        --input_path ./ \
                         --output_path .temp/parsed_pdbs.jsonl", shell=True)
         
-        subprocess.run("python make_pssm_dict.py", shell=True)
+        prep_mpnn.make_dict(self.seq_len)
         
-        # dictionaries to keep track of scores/sequences of mutated positions
-        sequences = defaultdict(list)
-        mutations = defaultdict(list)
-        scores = defaultdict(list)
-        
-        # get native sequence for future comparison
-        with open(f".temp/{self.name}.fasta", "r") as f:
-                native = f.readlines()[1]
-           
-        # iterating through sequence and mutating +_scoring one by one
-        for pos in range(1, seq_len+1):
-            
-            # fixed positions
-            fixed = ""
-            for i in range(1, seq_len+1):
-                if i != pos:
-                    fixed += f" {str(i)}"
-                    
-            # prepare MPNN 
-            subprocess.run(f"python ProteinMPNN/helper_scripts/make_fixed_positions_dict.py --input_path .temp/parsed_pdbs.jsonl \
-                --output_path .temp/fixed_pdbs.jsonl --chain_list A --position_list '{fixed}'", shell=True)
-            
-            # make sure you have the necessary files
-            assert os.path.exists('.temp/parsed_pdbs.jsonl') 
-            assert os.path.exists('.temp/pssm_dict.jsonl') 
-            assert os.path.exists('.temp/fixed_pdbs.jsonl') 
-                    
-            # Run MPNN
-            subprocess.run(f"python ProteinMPNN/protein_mpnn_run.py \
-                                --jsonl_path .temp/parsed_pdbs.jsonl\
-                                --out_folder ./.temp \
-                                --sampling_temp '0.1' \
-                                --pdb_path {self.target} \
-                                --pssm_jsonl .temp/pssm_dict.jsonl\
-                                --pssm_multi .8 \
-                                --num_seq_per_target 32 \
-                                --pssm_bias_flag 1 \
-                                --pssm_log_odds_flag 1 \
-                                --save_score 1 \
-                                --fixed_positions_jsonl .temp/fixed_pdbs.jsonl", shell=True)
-            
-            os.remove(".temp/fixed_pdbs.jsonl")
-            
-            # record scores
-            s = np.load(f"./.temp/scores/{self.name}.npz")
-            s = (s['score'].astype(str)).tolist() 
+        start = time.time()
 
-            ctr = -1  
-            for record in SeqIO.parse(f"./.temp/seqs/{self.name}.fa", "fasta"):
-                if ctr >= 0: # skip native sequence
-                    sequence = str(record.seq)
-                    
-                    for i in range(len(native)):  # ensure you are only recording sequences that have been mutated 
-                        if native[i] != sequence[i]:
-                            sequences[pos].append(sequence)
-                            mut = f"{native[i]}    {sequence[i]}" # record what native position has been mutated to
-                            mutations[pos].append(mut) 
-                            scores[pos].append(s[ctr])      
-                ctr += 1
+        # Run MPNN on sequence and mutating +_scoring one by one
+        print("Mutating positions one by one...")
+        pool = ThreadPool()
+        pool.map(self._task, range(self.seq_len))
+        pool.close()
+        pool.join()
         
-        data = []
-        for key in sequences.keys():
-            pos = key
-            
-            seqs = sequences[key]
-            muts = mutations[key]
-            s = scores[key]
-            
-            for i in range(len(seqs)):
-                data.append({
-                'position': key,
-                'sequence': seqs[i],
-                'scores': s[i],
-                'wt_resi': muts[i].split(" ")[0],
-                'mutated_resi': muts[i].split(" ")[-1]}
-            )
-                
-        df = pd.DataFrame(data)
-
-        # Rank mutations based on the score in descending order
-        df['scores'] = pd.to_numeric(df['scores'])  # Convert 'scores' column to numeric type
-        df2 = df.sort_values(by='scores', ascending=False).reset_index(drop=True)
-        df3 = df2.drop_duplicates('position', keep='first')
+        end = time.time()
         
-        os.makedirs('results',exist_ok=True)
-        df3.to_csv(f"results/{self.name}.csv", index=False)
-        
-        return df3.head(10)
+        return f"MPNN has finished running in {end-start} seconds, \
+            and all data has been recorded in .temp/csv"
    
-    def _getSeqs(self, mut: pd):
+   
+    def _getSeqs(self):
         
-        # get native sequence for future comparison
-        with open(f".temp/A0A0K8P6T7.fasta", "r") as f:
-            native = f.readlines()[1]
-            mut_seq = list(native)
+        all_data = {
+            "Position": [],
+            "Sequence": [],
+            "Wildtype": [],
+            "Mutant": [],
+            "Score": []
+        }
         
-        # create sequence with top 5 scoring mutations
-        muts = mut.head(5)
-        to_mutate = dict(zip(muts['position'], muts['mutated_resi']))
-        mpnn_seqs = []
-        for i in range(len(native)):
-            if (i+1) in to_mutate:
-                mut_seq[i] = to_mutate[i+1]
-        mpnn_seqs.append(''.join(mut_seq))
+        # iterate through the csv directory
+        filenames = listdir(".temp/csv")
+        files = [filename for filename in filenames if filename.endswith(".csv")]
         
-        # iteratively add next five mutations
-        iterative_muts = mut.tail(5)
-        to_mutate = dict(zip(iterative_muts['position'], iterative_muts['mutated_resi']))
-        for key in to_mutate:
-            mut_seq[key-1] = to_mutate[key]
-            mpnn_seqs.append(''.join(mut_seq))
+        for file in files:
+            all_data["Position"].append(int(file.replace(".csv","").split("_")[-1]) + 1)
+            with open(".temp/csv/"+file, "r") as f:
+                data = f.readlines()[0].split(",")
+                all_data["Sequence"].append(data[0])
+                all_data["Wildtype"].append(data[1].split(" ")[0])
+                all_data["Mutant"].append(data[1].split(" ")[-1])
+                all_data["Score"].append(float(data[2].replace("\n","")))
+        
+        df = pd.DataFrame.from_dict(all_data)
+        df.sort_values("Score", ascending=False, inplace=True,ignore_index=True)
+        
+        return df.head(10)
+    #     # create sequence with top 5 scoring mutations
+    #     muts = mut.head(5)
+    #     to_mutate = dict(zip(muts['position'], muts['mutated_resi']))
+    #     mpnn_seqs = []
+    #     for i in range(len(native)):
+    #         if (i+1) in to_mutate:
+    #             mut_seq[i] = to_mutate[i+1]
+    #     mpnn_seqs.append(''.join(mut_seq))
+        
+    #     # iteratively add next five mutations
+    #     iterative_muts = mut.tail(5)
+    #     to_mutate = dict(zip(iterative_muts['position'], iterative_muts['mutated_resi']))
+    #     for key in to_mutate:
+    #         mut_seq[key-1] = to_mutate[key]
+    #         mpnn_seqs.append(''.join(mut_seq))
             
-        return mpnn_seqs
+    #     return mpnn_seqs
 
 
-x = mpnnDesignTool("./example_run/A0A0K8P6T7.pdb")
+x = runMPNN("example_run/7nei.pdb")
 x.run()
-    
+
+
+# benchmark
+#TODO: install streamlit interface after benchmarking
